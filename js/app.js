9be71d67cc7c4913fc5da40c28e64b1249ca3f1f
taegorov <taegorov@gmail.com>
2021-03-15 16:58:21 -0700

    cleans up commented out code

--- js/app.js
+++ js/app.js
@@ -36,48 +36,27 @@ function hourlySales(min, max, avgcookies) {
 const seattle = {
     location: 'Seattle',
     hourlySales: hourlySales(23, 65, 6.3),
-    // [Math.random() * 6.3],
-    minCustomers: '23',
-    maxCustomers: '65',
-    avgCookieSale: '6.3',
-    cookiesPurchased: '', //per hour
 }
 
 
 const tokyo = {
     location: 'Tokyo',
     hourlySales: hourlySales(3, 24, 1.2),
-    minCustomers: '3',
-    maxCustomers: '24',
-    avgCookieSale: '1.2',
-    cookiesPurchased: '', //per hour
 }
 
 const dubai = {
     location: 'Dubai',
     hourlySales: hourlySales(11, 38, 3.7),
-    minCustomers: '11',
-    maxCustomers: '38',
-    avgCookieSale: '3.7',
-    cookiesPurchased: '', //per hour
 }
 
 const paris = {
     location: 'Paris',
     hourlySales: hourlySales(20, 38, 2.3),
-    minCustomers: '20',
-    maxCustomers: '38',
-    avgCookieSale: '2.3',
-    cookiesPurchased: '', //per hour
 }
 
 const lima = {
     location: 'Lima',
     hourlySales: hourlySales(2, 16, 4.6),
-    minCustomers: '2',
-    maxCustomers: '16',
-    avgCookieSale: '4.6',
-    cookiesPurchased: '', //per hour
 }
 
 const storeContainerElem = document.getElementById('store-container')
